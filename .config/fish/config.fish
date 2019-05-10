0537bb5f938f428d092a6b57fa09b0ab3de63d34
Alex V <alex.valiushko@whiteops.com>
2019-05-10 13:29:06 -0700

    update vars and tmux handling in fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,21 +1,16 @@
 set --erase fish_greeting
 
 # Dev variables
-set -Ux GOPATH $HOME/go
-set -Ux RUSTSRC (rustc --print sysroot)/lib/rustlib/src/rust/src
-set -Ux GOBIN $HOME/bin
-# set -Ux GO111MODULE on
-set -Ux ARCHFLAGS '-arch x86_64'
-set -Ux EDITOR vi
-set -Ux HOMEBREW_CASK_OPTS "--appdir=~/Applications"
+set -g GOPATH $HOME/go
+set -g RUSTSRC (rustc --print sysroot)/lib/rustlib/src/rust/src
+set -g GOBIN $HOME/bin
+set -g ARCHFLAGS '-arch x86_64'
+set -g EDITOR vi
+set -g HOMEBREW_CASK_OPTS "--appdir=~/Applications"
 
-set -U fish_user_paths /usr/local/opt/openssl/bin $HOME/bin $HOME/.cargo/bin $HOME/bin/FlameGraph > /dev/null 2> /dev/null or true
+set -g fish_user_paths $HOME/bin $HOME/.cargo/bin $HOME/bin/FlameGraph /usr/local/opt/openssl/bin /usr/local/opt/node@10/bin $HOME/.emacs.d/bin > /dev/null 2> /dev/null or true
 
 source $HOME/.config/fish/private-config.fish > /dev/null 2> /dev/null or true
-source $HOME/erls/18.1.5a/activate.fish > /dev/null 2> /dev/null or true
-
-# Command mode
-# fish_vi_key_bindings
 
 # Aliases
 alias g "git"
@@ -23,24 +18,17 @@ alias y "yadm"
 alias vi "nvim"
 alias vis "vise"
 alias rgg "rg --iglob '*.go' --iglob '!vendor'"
+alias tmux "tmux -2"
+alias venv "source ./env/bin/activate.fish"
 
-function tmux
-  command tmux -2 $argv
-end
-
-function speedtest
-  command echo (date) // (speedtest-cli --simple | sed 'N;s/\n/, /g') >> $HOME/speedtest.log
-end
-
-# Maintain a persistent Tmux session
+# Maintain a persistent tmux session
 if not set -q TMUX; and not set -q SSH_CONNECTION; and [ $TERM = 'xterm-256color' ]
     set tmux_session fish
-
-    if not tmux has-session ^/dev/null
+    if not tmux has-session 2>/dev/null
         tmux \
             start-server \; \
             new-session -d -s "$tmux_session" \; \
-            set-option -t "$tmux_session" destroy-unattached off >/dev/null ^/dev/null
+            set-option -t "$tmux_session" destroy-unattached off >/dev/null 2>/dev/null
     end
     exec tmux attach-session
 end
