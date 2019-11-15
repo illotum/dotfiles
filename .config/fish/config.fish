a1274e75ee60bb7cb0dfde51ae0a8fc0efec0232
Alex V <alex.valiushko@whiteops.com>
2019-11-14 16:28:25 -0800

    use kitty windows instead of tmux

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -22,13 +22,13 @@ alias tmux "tmux -2"
 alias venv "source ./env/bin/activate.fish"
 
 # Maintain a persistent tmux session
-if not set -q TMUX; and not set -q SSH_CONNECTION; and [ $TERM = 'xterm-256color' ]
-    set tmux_session fish
-    if not tmux has-session 2>/dev/null
-        tmux \
-            start-server \; \
-            new-session -d -s "$tmux_session" \; \
-            set-option -t "$tmux_session" destroy-unattached off >/dev/null 2>/dev/null
-    end
-    exec tmux attach-session
-end
+# if not set -q TMUX; and not set -q SSH_CONNECTION; and [ $TERM = 'xterm-256color' ]
+#     set tmux_session fish
+#     if not tmux has-session 2>/dev/null
+#         tmux \
+#             start-server \; \
+#             new-session -d -s "$tmux_session" \; \
+#             set-option -t "$tmux_session" destroy-unattached off >/dev/null 2>/dev/null
+#     end
+#     exec tmux attach-session
+# end
