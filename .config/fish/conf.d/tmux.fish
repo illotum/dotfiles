76fb6facfb7a382169d1d7954c831fe9341ce90e
Alex Valiushko <valiushk@amazon.com>
2022-06-01 04:08:53 +0000

    submodules are hell

--- .config/fish/conf.d/tmux.fish
+++ .config/fish/conf.d/tmux.fish
@@ -1,13 +1,14 @@
 alias tmux "tmux -2"
 
-# Maintain a persistent tmux session
-# if not set -q TMUX; and not set -q SSH_CONNECTION; and [ $TERM = 'xterm-256color' ]
-#     set tmux_session fish
-#     if not tmux has-session 2>/dev/null
-#         tmux \
-#             start-server \; \
-#             new-session -d -s "$tmux_session" \; \
-#             set-option -t "$tmux_session" destroy-unattached off >/dev/null 2>/dev/null
-#     end
-#     exec tmux attach-session
-# end
+return # temp disabled
+# Maintain a persistent tmux session over SSH
+if not set -q TMUX; and set -q SSH_CONNECTION; and [ $TERM = 'xterm-256color' ]
+    set tmux_session fish
+    if not tmux has-session 2>/dev/null
+        tmux \
+            start-server \; \
+            new-session -d -s "$tmux_session" \; \
+            set-option -t "$tmux_session" destroy-unattached off >/dev/null 2>/dev/null
+    end
+    exec tmux attach-session
+end
