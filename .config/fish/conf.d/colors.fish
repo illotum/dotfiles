4632154f6f6a82d9056780b27149a364c68d0a9c
Alex Valiushko <valiushk@amazon.com>
2022-05-31 19:37:36 -0700

    fish update

---
+++ .config/fish/conf.d/colors.fish
@@ -0,0 +1,29 @@
+set -U fish_color_autosuggestion brgreen
+set -U fish_color_cancel --reverse
+set -U fish_color_command brcyan
+set -U fish_color_comment brgreen
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_end blue
+set -U fish_color_error red
+set -U fish_color_escape cyan
+set -U fish_color_history_current --bold
+set -U fish_color_host normal
+set -U fish_color_host_remote yellow
+set -U fish_color_match --background=brblue
+set -U fish_color_normal normal
+set -U fish_color_operator cyan
+set -U fish_color_param brblue
+set -U fish_color_quote bryellow
+set -U fish_color_redirection brmagenta
+set -U fish_color_search_match 'bryellow'  '--background=black'
+set -U fish_color_selection 'white'  '--bold'  '--background=brblack'
+set -U fish_color_status red
+set -U fish_color_user brgreen
+set -U fish_color_valid_path --underline
+set -U fish_key_bindings fish_default_key_bindings
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description normal
+set -U fish_pager_color_prefix 'brcyan'  '--underline'
+set -U fish_pager_color_progress 'brwhite'  '--background=cyan'
+set -U fish_pager_color_selected_background --background=brblack
--- .config/fish/conf.d/mac.fish
+++ .config/fish/conf.d/mac.fish
@@ -5,6 +5,10 @@ set -U HOMEBREW_CASK_OPTS "--appdir=~/Applications"
 fish_add_path /usr/local/opt/make/libexec/gnubin
 fish_add_path /usr/local/opt/findutils/libexec/gnubin
 fish_add_path /usr/local/opt/openssl/bin
+fish_add_path /Applications/Emacs.app/Contents/MacOS/bin
+
+alias emacs="emacs -nw" # Always launch "emacs" in terminal mode.
+alias fs "fd -E build -E env -E node_modules -E site-packages -E release-info"
 
 set brew_asdf_path (brew --prefix asdf)
 if test -f $brew_asdf_path/libexec/asdf.fish
