7b18725a2610e43887a351506481f312f85cf264
Alex Valiushko <valiushk@amazon.com>
2022-05-31 19:38:14 -0700

    Groceries app

--- .config/nvim/filetype.lua
+++ .config/nvim/filetype.lua
@@ -5,6 +5,7 @@ end
 vim.filetype.add({
   extension = {
     escript = "erlang",
+    recipe  = "markdown",
     config  = { "erlang", { priority = 10 } },
     app     = { "erlang", { priority = 10 } },
 	conf    = { "systemd", { priority = 10 } }
