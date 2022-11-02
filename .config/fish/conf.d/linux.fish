98891f28115087ff5ab545951390e74f08f34a4b
Alex Valiushko <valiushk@amazon.com>
2022-11-02 17:04:41 +0000

    linux update

--- .config/fish/conf.d/linux.fish
+++ .config/fish/conf.d/linux.fish
@@ -1,5 +1,10 @@
 test (uname) = "Linux"; or return
 
+fish_add_path /usr/share/bcc/tools/
+fish_add_path /usr/local/go/bin
+
+set -xg TMPDIR /tmp
+
 if test -f ~/.asdf/asdf.fish
 	source ~/.asdf/asdf.fish
 end
