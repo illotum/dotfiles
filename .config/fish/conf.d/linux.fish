cfb33a520b93c6fcd02b3f9af238ab4c4ed08cdd
Alex Valiushko <illotum@gmail.com>
2025-07-30 15:31:41 -0700

    sudo gcloud

--- .config/fish/conf.d/linux.fish
+++ .config/fish/conf.d/linux.fish
@@ -4,3 +4,4 @@ fish_add_path /usr/share/bcc/tools/
 fish_add_path $HOME/.local/bin
 
 set -xg TMPDIR /tmp
+set -xg GOOGLE_APPLICATION_CREDENTIALS $HOME/.config/gcloud/application_default_credentials.json
