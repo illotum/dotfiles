869f918cb5e7294e129b730fcaa58930798dac04
Alex Valiushko <illotum@gmail.com>
2025-08-05 15:21:41 -0700

    mise & gemini

--- .config/fish/conf.d/env.fish
+++ .config/fish/conf.d/env.fish
@@ -2,6 +2,9 @@ set -gx EDITOR hx
 set -gx XDG_CONFIG_HOME $HOME/.config
 
 test (command -v zoxide); and zoxide init fish | source
+test (command -v mise); and mise activate fish | source
+
+set -gx GOOGLE_CLOUD_PROJECT gemini-ai-sandbox
 
 alias y "git --git-dir=$HOME/.dotfiles/.git --work-tree=$HOME"
 abbr g git
