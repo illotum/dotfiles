3c76ddfeb0330297d42a04a35992511d187d334c
Alex Valiushko <illotum@gmail.com>
2025-10-06 16:57:34 -0700

    work env updates

--- .config/fish/conf.d/go.fish
+++ .config/fish/conf.d/go.fish
@@ -1,5 +1,5 @@
 set -gx GOPRIVATE "bitbucket.surfcrew.com,gitlab.menloinfra.com"
-set -gx GOPROXY "https://us-west1-go.pkg.dev/fond-giraffe-1d7l/go"
+set -gx GOPROXY "https://oauth2accesstoken:$(gcloud auth print-access-token)@us-west1-go.pkg.dev/fond-giraffe-1d7l/go,https://proxy.golang.org,direct"
 set -gx GOBIN $HOME/bin
 fish_add_path $HOME/bin
 
--- .config/fish/conf.d/rust.fish
+++ .config/fish/conf.d/rust.fish
@@ -1,4 +1,3 @@
-test (command -v rustc >/dev/null); or return
-set -gx RUSTSRC (rustc --print sysroot)/lib/rustlib/src/rust/src
+test (command -v $HOME/.cargo/bin/rustc >/dev/null); or return
 set -gx CARGO_INSTALL_ROOT $HOME
 fish_add_path $HOME/.cargo/bin
