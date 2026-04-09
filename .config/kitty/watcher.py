30470c2508bc9f1c984cd5c3d14d11b521bc506c
Alex Valiushko <illotum@gmail.com>
2026-04-09 09:04:54 -0700

    tweak helix theme delivery

--- .config/kitty/watcher.py
+++ .config/kitty/watcher.py
@@ -11,12 +11,8 @@ def on_color_scheme_preference_change(boss: Boss, window: Window, data: dict[str
     # light to dark or vice versa. data contains is_dark and via_escape_code
     # the latter will be true if the color scheme was changed via escape
     # code received from the program running in the window
-    with Path.home() / ".config" / "helix" / "themes" as path:
-        link_path = str(path / "solarized.toml")
+    with open(Path.home() / ".config" / "helix" / "themes" / "solarized.toml") as f:
+        theme = "solarized_light"
         if data['is_dark']:
-            theme_path = str(path / "solarized_dark.toml")
-        else:
-            theme_path = str(path / "solarized_light.toml")
-        if os.path.isfile(link_path):
-            os.remove(link_path)
-        os.symlink(theme_path, link_path)
+            theme = "solarized_dark"
+        f.write(f"inherits = '{theme}'\n")
